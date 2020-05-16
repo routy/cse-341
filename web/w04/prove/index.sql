74aa1e0a25eae4a0b257337490036c86306768a7
Nick Routsong <nr@futuramicmedia.com>
2020-05-16 11:36:24 -0700

    Adding in sql files

---
+++ web/w04/prove/index.sql
@@ -0,0 +1,13 @@
+/**
+
+- This system must track notes for multiple users.
+
+- The speakers should be tracked separately from the conference because one speaker will speak at multiple conferences.
+
+- Individual sessions of conference (e.g., Saturday morning, Sunday afternoon) need not be tracked, rather simply the year and month, (e.g., April 2009, Oct 2012).
+
+- You need not account for the fact that some speakers will speak multiple times at a conference, but assume that a note can be tied to a "speaker + conference."
+
+- Multiple notes can be added by the same user about a single conference talk.
+
+ */
