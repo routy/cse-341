f741338e5b0c324afadcbbc6734ee88688dadd27
Nick Routsong <nr@futuramicmedia.com>
2020-05-18 23:19:39 -0700

    Adding connection to postgres db from php w05

---
+++ web/w05/team/index.php
@@ -0,0 +1,29 @@
+<?php
+
+$uri = getenv('DATABASE_URL');
+
+try {
+    
+    $db = parse_url( $uri );
+
+    $db = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s;sslmode=require",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $query = "SELECT * FROM users";
+
+    $statement = $db->query( $query );
+    $results   = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+} catch( PDOException $e ) {
+
+    echo $e->getMessage();
+
+}
